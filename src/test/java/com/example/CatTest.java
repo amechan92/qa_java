d618b1b90646a2ae7e4a959e6e40c595eb2ca943
Elena Aleksandrova <himitsu1892@gmail.com>
2024-04-25 00:18:15 +0500

    Добавлены тесты

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class CatTest {
+
+    @Mock
+    private Feline feline;
+
+    private Cat cat;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        cat = new Cat(feline);
+    }
+
+    @Test
+    public void testCatGetSound() {
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testCatGetFood() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        when(feline.eatMeat()).thenReturn(expectedFood);
+        assertEquals(expectedFood, cat.getFood());
+    }
+}
